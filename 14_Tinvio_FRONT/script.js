edebcc5fe857f861d89f7ff480affcd520a7ab47
DepZ0 <depzo221@gmail.com>
2023-09-09 10:34:59 +0300

    1

--- 14_Tinvio_FRONT/script.js
+++ 14_Tinvio_FRONT/script.js
@@ -62,3 +62,103 @@ document.querySelector('.block_3_b_payments').onclick = () => {
     //     document.querySelector('.menu').style.display = 'none';
     // }
 // ---------- MENU --------------
+
+
+// ---------- Selector Of Language --------------
+
+
+let isLanguageListOpen = false;
+
+document.querySelector('.languageActive').onclick = () => {
+    const languageList = document.querySelector('.languageList');
+    const languageSelectorArrow = document.querySelector('.languageSelectorArrow');
+    
+    if (isLanguageListOpen) {
+        // Если список открыт, закрываем его
+        languageList.style.display = 'none';
+        languageSelectorArrow.style.transform = 'rotate(0deg)';
+        isLanguageListOpen = false;
+    } else {
+        // Если список закрыт, открываем его
+        languageList.style.display = 'block';
+        languageSelectorArrow.style.transform = 'rotate(180deg)';
+        isLanguageListOpen = true;
+    }
+}
+
+let selectedLanguage = 'en';
+const languageList = ['.languageBritannia', '.languagePoland', '.languageThailand', '.languageChina'];
+
+document.querySelector(languageList[0]).onclick = () => {
+    document.querySelector('.languageActive').textContent = 'EN';
+    document.querySelector('.languageList').style.display = 'none';
+    document.querySelector('.languageSelectorArrow').style.transform = 'rotate(0deg)';
+
+    isLanguageListOpen = false;
+    selectedLanguage = 'en'
+}
+
+document.querySelector(languageList[1]).onclick = () => {
+    document.querySelector('.languageActive').textContent = 'PL';
+    document.querySelector('.languageList').style.display = 'none';
+    document.querySelector('.languageSelectorArrow').style.transform = 'rotate(0deg)';
+
+    isLanguageListOpen = false;
+    selectedLanguage = 'pl'
+}
+
+document.querySelector(languageList[2]).onclick = () => {
+    document.querySelector('.languageActive').textContent = 'TH';
+    document.querySelector('.languageList').style.display = 'none';
+    document.querySelector('.languageSelectorArrow').style.transform = 'rotate(0deg)';
+
+    isLanguageListOpen = false;
+    selectedLanguage = 'th'
+}
+
+document.querySelector(languageList[3]).onclick = () => {
+    document.querySelector('.languageActive').textContent = 'CH';
+    document.querySelector('.languageList').style.display = 'none';
+    document.querySelector('.languageSelectorArrow').style.transform = 'rotate(0deg)';
+
+    isLanguageListOpen = false;
+    selectedLanguage = 'ch'
+}
+
+
+
+// ---------- SLIDER -----------
+
+const imagesSqueres = document.querySelectorAll('.block_5_squere_for_slides');
+let count = 0; // переменная, которая указывает на номер активного изображения в images
+
+const next = document.querySelector('.block_5_slider_button_right');
+next.onclick = nextFunction;
+
+const prev = document.querySelector('.block_5_slider_button_left');
+prev.onclick = prevFunction;
+
+function nextFunction() {
+if (count + 1 < imagesSqueres.length) {
+  count++;
+} else {count = 0;}
+for ( i = 0; i < imagesSqueres.length; i++) {
+    imagesSqueres[i].classList.remove('block_5_squre_active');
+}
+// -------------
+imagesSqueres[count].classList.add('block_5_squre_active');
+
+document.querySelector('.block_5_slides').src = `public/block_5_slide_${count}.svg`;
+}
+
+function prevFunction() {
+  if (count - 1 >= 0) {
+    count--;
+  } else {count = imagesSqueres.length - 1;}
+  for (i = 0; i < imagesSqueres.length; i++) {
+    imagesSqueres[i].classList.remove('block_5_squre_active');
+  }
+  // -------------
+  imagesSqueres[count].classList.add('block_5_squre_active');
+  document.querySelector('.block_5_slides').src = `public/block_5_slide_${count}.svg`;
+}
